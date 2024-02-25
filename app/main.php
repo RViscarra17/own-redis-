e078d2fae9e29c3dedd1c7034788b706781a6be1
Rafael Viscarra <rafael.viscarra97@hotmail.com>
2024-02-25 17:43:13 -0600

    pass 2nd stage

--- app/main.php
+++ app/main.php
@@ -9,7 +9,14 @@ $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
 socket_bind($sock, "localhost", 6379);
 socket_listen($sock, 5);
-socket_accept($sock); // Wait for first client
+$client = socket_accept($sock); // Wait for first client
+$pingRequest = "ping";
+$request = socket_read($client, strlen($pingRequest));
+
+if ($request) {
+    $pongResponse = "PONG\r\n";
+    socket_write($client, $pongResponse, strlen($pongResponse), 0);
+}
 
 // socket_close($sock);
 ?>
